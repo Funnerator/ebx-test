0821e3b31535c1ba8da3c0324c125747807782ce
Alex Bullard <abullrd@gmail.com>
2013-09-10 13:39:54 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beanstalk::Application.config.secret_token = '69f97fce87bd606b9a7025834475844d7c0a7f40974aa71d0a6b6ed189399d39432fb451e1778ad52a38f325702bd0144951f96fbc245f4a75b68235355f9190'
